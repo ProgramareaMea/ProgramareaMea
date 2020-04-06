f4c945a68a9a355e7b84e645b405d7659e90df14
SaladBreaker <lucavlad50@gmail.com>
2020-04-06 22:21:32 +0300

    Added skeleton for AdminCodes class

--- app/src/main/java/com/tp/programareamea/firebase/AdminCodes.kt
+++ app/src/main/java/com/tp/programareamea/firebase/AdminCodes.kt
@@ -1,4 +1,17 @@
 package com.tp.programareamea.firebase
 
-class AdminCodes {
+class AdminCodes constructor(inputCode: String) {
+    private var userInputCode: String = inputCode
+
+    fun setInputCode(code:String){
+        this.userInputCode = code
+    }
+
+    fun checkCodeIsValid(): Boolean {
+        return this.getAdminCodes().contains(this.userInputCode)
+    }
+
+    private  fun getAdminCodes(): List<String> {
+        return listOf<String>("aaa","cdsf","dsa")
+    }
 }
--- app/src/main/java/com/tp/programareamea/firebase/User.kt
+++ app/src/main/java/com/tp/programareamea/firebase/User.kt
@@ -7,7 +7,7 @@ data class User(
     var uid: String="",
     var name: String="",
     var email: String="",
-    var user_type: String="",
+    var userType: String="",
     @get:Exclude var isNew: Boolean = true,
     @get:Exclude var isCreated: Boolean = true,
     @get:Exclude var isAuthenticated:Boolean = false
