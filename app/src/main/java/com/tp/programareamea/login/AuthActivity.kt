813fcd62c4c54bf252ce50813f2b0bf626da1bdb
SaladBreaker <lucavlad50@gmail.com>
2020-04-11 17:41:57 +0300

    Some progress

--- app/src/main/java/com/tp/programareamea/login/AuthActivity.kt
+++ app/src/main/java/com/tp/programareamea/login/AuthActivity.kt
@@ -3,16 +3,14 @@ package com.tp.programareamea.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.tp.programareamea.R
 
-
-
 class AuthActivity : AppCompatActivity() {
-
+    private lateinit var binding: AppCompatActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
 }
