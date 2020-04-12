c361e1097cb5b1e0628d18b2607cffa5c9f65c76
SaladBreaker <lucavlad50@gmail.com>
2020-04-12 12:52:15 +0300

    Button bug

--- app/src/main/java/com/tp/programareamea/login/AuthActivity.kt
+++ app/src/main/java/com/tp/programareamea/login/AuthActivity.kt
@@ -3,14 +3,12 @@ package com.tp.programareamea.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.tp.programareamea.R
 
 class AuthActivity : AppCompatActivity() {
-    private lateinit var binding: AppCompatActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
     }
 
 }
